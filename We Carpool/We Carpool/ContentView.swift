a76a72517949269ff9270dfe370c6eeca2c0efc5
Kunapuli Vaishnavi <kunapuli.vaishnavi@gmail.com>
2019-11-22 17:56:11 -0800

    password changed

--- We Carpool/We Carpool/ContentView.swift
+++ We Carpool/We Carpool/ContentView.swift
@@ -4,8 +4,8 @@
 import SwiftUI
 let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
  
-let storedUsername = "A"
-let storedPassword = "B"
+let storedUsername = "a"
+let storedPassword = "b"
  
 struct ContentView: View {
     @State var username: String = ""
